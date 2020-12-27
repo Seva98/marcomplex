b9480ee95e17a0ace40cb374c357348358e7859c
Ondrej Sevcik <Ondrej Sevcik>
2020-12-27 16:45:27 +0100

    2020-12-27 update

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+# Master
+yarn build
+git add -A
+git commit -m "`date +'%Y-%m-%d'` update"
+git push origin main
+# gh-pages
+#sed -i '' '/^_site/d' .gitignore
+git commit -a -m "`date +'%Y-%m-%d'` update"
+git push origin `git subtree split --prefix _site master --rejoin`:refs/heads/gh-pages --force
+#echo "_site" >> .gitignore
